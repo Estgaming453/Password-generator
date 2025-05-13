a271e94caa12911c02212f0ec50231364ce09bee
Estgaming453 <163151261+Estgaming453@users.noreply.github.com>
2025-05-13 02:24:37 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+function generatePassword() {
+ const lowercase = 'abcdefghijklmnopqrstuvwxy';
+ const uppercase ='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+ const numbers = '0123456789'
+ const symbols ='!@#$%^&*()_+-=[]{]|;:,.<>?';
+
+
+let charset ='';
+if (document.getElementById('lowercase').checked){
+    charset += lowercase;
+}
+if (document.getElementById('uppercase').checked){
+    charset += uppercase;
+}
+if (document.getElementById('numbers').checked){
+    charset += numbers;
+}
+if (document.getElementById('symbols').checked){
+    charset += symbols;
+}
+if(charset.length === 0){
+    alert('Veuillez séléctionner au moins un type de caractère.');
+    return;
+}
+const length = parseInt(document.getElementById('length').value);
+if (length < 4 || length > 24){
+    alert('La longueur doit être entre 4 et 24 caratères.');
+    return;
+}
+let password ='';
+for (let i = 0; i < length; i++){
+    const randomIndex =Math.floor(Math.random()* charset.length);
+    password += charset[randomIndex];
+}
+document.getElementById('password').value = password;
+}
+ function copyPassword(){
+    const password = document.getElementById('password');
+    if(password.value){
+        navigator.clipboard.writeText(password.value).then(() => {
+            alert('Mot de passe copié dans le presse-papiers !');
+            }).catch(() => {
+                alert('échec de la copie du mot de passe.')
+            });
+    }else{
+        alert('Aucun mot de passe à copier !')
+    }
+        
+   
+    }
+ 
